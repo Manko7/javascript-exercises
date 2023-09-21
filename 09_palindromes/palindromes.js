c0220defdceba3fc06195664bab103ff4385d329
Manko7 <35254355+Manko7@users.noreply.github.com>
2023-09-21 21:42:48 +0200

    back on track

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let temp = [];
+    for(let i = 0; i < string.length; i++) {
+        ("!" === string[i]) ? "" : temp.append();
+        console.log(temp.join().toLowerCase());
+    }
+    console.log(temp.join().toLowerCase());
+    return (temp.join().toLowerCase() === temp.split("").reverse().join("").toLowerCase() ? true : false)
 };
 
 // Do not edit below this line
