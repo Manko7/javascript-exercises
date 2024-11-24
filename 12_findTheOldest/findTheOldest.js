55beaef37b106ac14cab00f52cc97670b059ce23
Manko7 <35254355+Manko7@users.noreply.github.com>
2024-11-24 01:04:55 +0100

    exercising

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,61 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(value) {
+    let currentYear = new Date().getFullYear()
+    let oldest = 0
+    let count = 0
+    // for(p in arr) {
+    //     let age = 0
+    //     if(p.yearOfDeath) {
+    //         console.log(p.yearOfDeath)
+    //         console.log(currentYear)
+    //         age = p.yearOfDeath - p.yearOfBirth
+    //     } else {
+    //         console.log(p.yearOfDeath)
+    //         console.log(currentYear)
+    //         age = currentYear - p.yearOfBirth
+    //     }
+    //     if (oldest < Number(age)) {
+    //         oldest = count;
+    //     }
+    //     count += 1
+    // }
+    // return arr[oldest].name
+    for(p in value) {
+        console.log(p)
+        console.log(p.name)
+        console.log(p.yearOfDeath)
+        console.log(p.yearOfBirth)
+    }
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+    {
+      name: "Manko",
+      yearOfBirth: 1912,
+    },
+  ]
+
+findTheOldest(people);
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
